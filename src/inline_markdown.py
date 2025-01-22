4050f4a577ba2ae992c896229a17696c7a77856c
pranay0205 <pranayghuge@gmail.com>
2025-01-22 14:26:13 -0500

    add extract links and images function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,6 @@
 from typing import List
 from textnode import TextType, TextNode
+import re
 
 
 def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: TextType):
@@ -27,3 +28,22 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: 
         result.extend(splitted_nodes)
 
     return result
+
+
+def extract_markdown_images(text: str):
+    regex = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex, text)
+    return matches
+
+
+def extract_markdown_links(text: str):
+    regex = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(regex, text)
+    return matches
+
+
+text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+extract_markdown_images(text)
+
+text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+x = extract_markdown_links(text)
