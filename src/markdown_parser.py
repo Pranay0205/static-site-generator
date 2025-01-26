d91e3a32f56de73ccd5f4eedfb3892260b51c421
pranay0205 <pranayghuge@gmail.com>
2025-01-26 18:30:43 -0500

    add parse_paragraph, parse_heading, parse_unordered_list function

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,48 +1,77 @@
 from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 from inline_markdown import text_to_textnodes
-from textnode import TextType, text_node_to_html_node
+from textnode import text_node_to_html_node
 
 
-def create_html_heading(text):
-    level = text.count("#")
-    return HTMLNode(f"h{level}", text.strip("# "))
-
-
-def text_to_children(block):
+def parse_heading(block):
     htmlnodes = []
+    parent = None
+
     for line in block.split("\n"):
-        parentnode = None
         textnodes = text_to_textnodes(line)
         for textnode in textnodes:
             if textnode.text.startswith("#"):
-                parentnode = create_html_heading(textnode.text)
+                level = textnode.text.count("#")
+                textnode.text = textnode.text.strip("# ")
+                parent = ParentNode(
+                    f"h{level}", [text_node_to_html_node(textnode)])
+                htmlnodes.append(parent)
             else:
                 htmlnode = text_node_to_html_node(textnode)
-                parentnode.children = htmlnode
+                if parent:
+                    parent.children.append(htmlnode)
+                else:
+                    htmlnodes.append(htmlnode)
 
-    return [parentnode]
+    return [htmlnodes]
 
 
-def markdown_to_paragraph_node(markdown):
-    htmlnode = HTMLNode("p", markdown)
-    # print(htmlnode)
+def parse_paragraph(block):
+    textnodes = text_to_textnodes(block)
+    for i, textnode in enumerate(textnodes):
+        if i == 0:
+            htmlnode = ParentNode("p", [text_node_to_html_node(textnode)])
+        htmlnode.children.append(text_node_to_html_node(textnode))
+    return [htmlnode]
+
+
+def parse_unordered_list(block):
+
+    textnodes = []
+    for line in block.split("\n"):
+        if line.startswith("*") or line.startswith("-"):
+            textnodes.extend(text_to_textnodes(line.strip()[1:].strip()))
+
+    children = ParentNode("li", [])
+
+    for textnode in textnodes:
+        children.children.append(text_node_to_html_node(textnode))
+
+    return ParentNode("ul", [children])
+
+
+def parse_ordered_list(block):
+    pass
 
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    htmlnodes = []
     for block in blocks:
         block_type = block_to_block_type(block)
         if block_type == BlockType.heading:
-            text_to_children(block)
+            htmlnodes.extend(parse_heading(block))
         if block_type == BlockType.paragraph:
-            markdown_to_paragraph_node(block)
+            htmlnodes.extend(parse_paragraph(block))
+        if block_type == BlockType.unordered_list:
+            print(parse_unordered_list(block))
 
 
 markdown_to_html_node("""## This is a **heading**\n\n### This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
-* This is the first list item in a list block
-* This is a list item
-* This is another list item""")
+* First is the first list **item** in a list block
+* Second is a list item
+* Third is another list item""")
